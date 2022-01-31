7068795181c09889c5457d086bf7aaf6ea254056
Alan Miranda <ti040@irmaosgoncalves.com.br>
2022-01-30 20:33:20 -0400

    Revisão dos vídeos do Eder

--- fundamentos/inicio/sentenca.js
+++ fundamentos/inicio/sentenca.js
@@ -2,3 +2,9 @@
 console.log("Bom dia!");
 console.log("Boa tarde!");
 console.log("Boa noite!");
+
+// Revisão dos vídeos do Eder
+https://drive.google.com/drive/folders/16dRIoFup2KdjXRiiGcVv1VRNST4va48d
+Vuex.mp4
+VueRouter.mp4
+Video 1.mp4
